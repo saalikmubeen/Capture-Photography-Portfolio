2297232699e4d9ccd383e5c7953e1f2d3aaf74a2
saalik123 <63722568+saalik123@users.noreply.github.com>
2020-12-24 16:11:41 +0530

    styled faq component

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 
 
 const Faq = () => {
     return (
-        <div className="faq">
+        <StyledFaq>
             <h2>Any Questions? <span>FAQ</span></h2>
             <div className="faq-questions">
-                <div className="question">
-                    <p>How Do I Start?</p>
+                <div>
+                    <p className="question">How Do I Start?</p>
                     <div className="answer">
                         <p>Lorem ipsum dolor sit amet.</p>
                         <p>
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
-                            autem accusamus ex laboriosam porro, adipisci quam voluptatum
-                            magnam placeat corporis.
+                            autem accusamus ex laboriosam porro, adipisci quam.
                         </p>
                     </div>
+                    <div className="faq-line" />
                 </div>
-                <div className="question">
-                    <p>What Products Do You Offer?</p>
+                <div>
+                    <p className="question">What Products Do You Offer?</p>
                     <div className="answer">
                         <p>Lorem ipsum dolor sit amet.</p>
                         <p>
@@ -27,9 +28,10 @@ const Faq = () => {
                         magnam placeat corporis.
                         </p>
                     </div>    
+                    <div className="faq-line" />
                 </div>
-                <div className="question">
-                    <p>Different Payment Methods</p>
+                <div>
+                    <p className="question">Different Payment Methods</p>
                     <div className="answer">
                         <p>Lorem ipsum dolor sit amet.</p>
                         <p>
@@ -38,9 +40,10 @@ const Faq = () => {
                             magnam placeat corporis.
                         </p>
                     </div>
+                    <div className="faq-line" />
                 </div>
-                <div className="question">
-                    <p>Daily Schedule</p>
+                <div>
+                    <p className="question">Daily Schedule</p>
                     <div className="answer">
                         <p>Lorem ipsum dolor sit amet.</p>
                         <p>
@@ -49,11 +52,47 @@ const Faq = () => {
                             magnam placeat corporis.
                         </p>
                     </div>
+                    <div className="faq-line" />
                 </div>
             </div>        
-        </div>
+        </StyledFaq>
     )
 }
 
+
+const StyledFaq = styled.div`
+    min-height: 100vh;
+    display: block;
+    padding: 5rem 10rem;
+    color: white;
+    span {
+        display: block;
+    }
+
+    h2 {
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line {
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question {
+        padding: 1.5rem 0rem;
+        cursor: pointer;
+        color: white;
+        font-size: 1.8rem;
+        font-weight: bold;
+    }
+    .answer {
+        padding: 2rem 0rem;
+        p {
+            padding: 1rem 0rem;
+            font-size: 1.1rem;
+        }
+}
+`
 
 export default Faq;
