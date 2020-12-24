aa0dc9ca6b837ab45e2318b9c573566c04a99c38
saalik123 <63722568+saalik123@users.noreply.github.com>
2020-12-24 16:36:03 +0530

    added nav component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import GlobalStyles from './components/GlobalStyles'; 
+import Nav from './components/Nav';
 import AboutPage from './pages/AboutPage';
 import WorkPage from './pages/WorkPage';
 import ContactPage from './pages/ContactPage';
@@ -9,7 +10,8 @@ import ContactPage from './pages/ContactPage';
 const App = () => {
     return (
         <>
-        <GlobalStyles/>
+            <GlobalStyles />
+            <Nav/>
         <Switch>
             <Route path="/" exact component={AboutPage} />
             <Route path="/my-work" exact component={WorkPage} />
---
+++ src/components/Nav.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+
+const Nav = () => {
+    return (
+        <StyledNav>
+            <h1 id="logo"><Link to="/">Capture</Link></h1>
+            <ul>
+                <li><Link to="/">1. About Us</Link></li>
+                <li><Link to="/my-work">2. Work</Link></li>
+                <li><Link to="/contact">3. Contact Us</Link></li>
+            </ul>
+        </StyledNav>
+    )
+}
+
+
+const StyledNav = styled.nav`
+    min-height: 10vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1rem 10rem;
+    background: #282828;
+    position: sticky;
+    top: 0;
+
+    a {
+    color: white;
+    text-decoration: none;
+    }
+
+    #logo a{
+        font-size: 1.5rem;
+        font-family: "Lobster", cursive;
+        font-weight: lighter;
+        color: white;
+    }
+
+    ul{
+        display: flex;
+        list-style: none
+    }
+
+    li {
+    color: white;
+    text-decoration: none;
+    padding-left: 5rem;
+    a {
+        font-size: .9rem;
+    }
+    }
+`
+
+
+
+
+export default Nav;
